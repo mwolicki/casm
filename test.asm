021547b7b848cf7d08463a460a7f65a3de5f508b
Marcin Wolicki <marcin.wolicki@gmail.com>
2018-11-07 21:47:33 -0800

    escape quote example

---
+++ test.asm
@@ -0,0 +1,9 @@
+!org 0x1234
+dasdas asd "asd 
+    \\"sdhfjksdhkf\"
+safsd"
+
+    #    hdsajkjds aslkjdhkljjahs askljdjklashjk klsajhdjkhas
+
+a b c # ala ma kota
+ a          b        c         0x1114324
