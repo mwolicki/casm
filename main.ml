007a4786d26bbe59c5522823591d52a2d7021e55
Marcin Wolicki <marcin.wolicki@gmail.com>
2018-11-11 09:14:35 -0800

    extracted tokenizer to new module

--- main.ml
+++ main.ml
@@ -1,5 +1,7 @@
 open Parser
+open Tokenizer
 open Stdint
+
 let load_file filename : string =
     let lines = ref [] in
     let chan = open_in filename in
@@ -11,53 +13,13 @@ let load_file filename : string =
         close_in chan;
         !lines |> List.rev |> String.concat "\n"
 
-type name = string
-type arg = Ident of string | Numb of int64
 
-type op = string
 
-type tokens =
-| Op of op * arg list
-| Label of name
-| Directive of name * arg list
-
-let ast_to_string = function
-| Op (op, args) -> 
-    let args = args |> List.map (function Numb i -> Int64.to_string i | Ident i -> i) in
-    op ^ " (" ^ (String.concat ", " args ) ^ ")" 
-| Directive (op, args) -> 
-    let args = args |> List.map (function Numb i -> Int64.to_string i | Ident i -> i) in
-    "!" ^ op ^ " (" ^ (String.concat ", " args ) ^ ")" 
-| Label name -> name ^ ":"
-
-let is = function
-| Ok (i,_) -> "YAY " ^ (ast_to_string i) 
-| Error r -> "noooop " ^ r
-
-let comment = pChar '#' >>> pAll2(pNotChar '\n') @=> ignore
-let pArg = pWhitespace >>> ((pIdent @=> fun a -> Ident a) ||| (pInteger @=> fun a -> Numb a) ||| (pStringLiteral '"'  @=> fun a -> Ident a))
-let directive = 
-    (pZeroWhitespace >>> pChar '!' >>> pString <|> pSep pArg ',' <<< pZeroWhitespace) @=> fun (name, args)-> Directive (name, args)
-let op = 
-    ((pZeroWhitespace >>> pIdent <|> pSep pArg ',' <<< pZeroWhitespace) @=> fun (name, args)-> Op (name, args))
-    ||| (pZeroWhitespace >>> pIdent) @=> fun name -> Op(name, [])
-let label = (pZeroWhitespace >>> pChar ':' >>> pIdent) @=> fun name -> Label name
-let newLineAndEmptyLines = pAll2(pZeroWhitespace <<<? comment <<< pChar '\n')
-let line = directive ||| op ||| label
-let lines = 
-    refl(fun lines -> (((line <<< newLineAndEmptyLines) <|> lines) @=> fun (a,b) -> a::b) ||| (line @=> fun a -> [a]))
-
-let pFile = lines <<< pZeroAll2(pChoose [pIgnore pWhitespace; pIgnore comment;  pIgnore (pChar '\n')])
-
-let int_size = function
-    | `BYTE -> 1
-    | `WORD -> 2
-    | `DWORD -> 4
-    | `QWORD -> 8
+let size_to_int = function `BYTE -> 1 | `WORD -> 2 | `DWORD -> 4 | `QWORD -> 8
 
 let emit_data size = 
     let to_bytes (v:Int64.t) = 
-        let buff = Bytes.create (int_size size) in
+        let buff = Bytes.create (size_to_int size) in
         let () =
             match size with 
             | `BYTE -> Int8.to_bytes_little_endian (Int8.of_int64 v) buff 0
@@ -80,6 +42,7 @@ let emmit = function
 | Op ("dd", args) -> emit_data `DWORD args
 | Op ("dq", args) -> emit_data `QWORD args
 | Directive _ -> (*Ignore for now *) Bytes.empty
+| Label _ -> (*Ignore for now *) Bytes.empty
 | x -> failwith ("Unknown ast element " ^ (ast_to_string x))
 
 let () =
